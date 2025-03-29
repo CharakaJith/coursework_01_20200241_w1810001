06641eb7585b3d87b652131123255c5d39dd4e45
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-03-29 23:32:35 +0530

    feature - ui implemented with React

---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="fixed-navbar">
+      <div className="logo">
+        <h1>MyWebsite</h1>
+      </div>
+      <div className="nav-center">
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="auth-links">
+        <Link to="/login" className="login-link">
+          Login
+        </Link>
+        <Link to="/signup" className="signup-link">
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
