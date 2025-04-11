13baecb764fe36922caa9796fbc57f371fe21345
Charaka Gunasinghe <charaka.info@gmail.com>
2025-04-12 02:51:22 +0530

    feature - ui - home page implemented

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -5,8 +5,14 @@ import Navbar from '../../Components/navbar/navbar';
 function HomePage() {
   return (
     <div>
-      {<Navbar />}
-      {<Footer />}
+      <Navbar />
+      <h1 className="slogan">
+        Discover the world,
+        <br />
+        one country at a time!
+      </h1>
+      <p className="slogan-subtext">Register now to explore detailed info about every country!</p>
+      <Footer />
     </div>
   );
 }
