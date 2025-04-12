4a90837ce8c12ff2a09a769972a27ee76b52841f
Charaka Gunasinghe <charaka.info@gmail.com>
2025-04-12 23:50:21 +0530

    fix - ui - added get country bu id to endpoint page

--- client/src/Components/endpoint-display/endpointDisplay.js
+++ client/src/Components/endpoint-display/endpointDisplay.js
@@ -10,12 +10,12 @@ function EndpointDisplay() {
         <h1>API Endpoints</h1>
       </div>
 
-      {/* endpoints */}
+      {/* get all countries endpoint */}
       <div className="endpoint-box">
         <div className="side-by-side-boxes">
           {/* left box */}
           <div className="box-left">
-            <h2>Retrieve country details</h2>
+            <h2>Retrieve all countries</h2>
             <p>
               Retrieves the details of a specific country, including information such as its name, population, currency, and region. The country is
               identified based on the provided country code or name. For a sample request, see Fetching country data by name or code.
@@ -31,7 +31,7 @@ function EndpointDisplay() {
 
             <h3>Returns</h3>
             <hr />
-            <p>Returns a country details object for the specified country, including name, population, currency, and region.</p>
+            <p>Returns a list of countries with details including names, capital, languages, flag, currency, and timestamps.</p>
           </div>
 
           {/* right box */}
@@ -66,7 +66,7 @@ function EndpointDisplay() {
                 "data": {
                         "countries": [
                                 {
-                                        "id": 241,
+                                        "id": 1,
                                         "officialName": "American Samoa",
                                         "commonName": "American Samoa",
                                         "commonName": "American Samoa",
@@ -95,6 +95,97 @@ function EndpointDisplay() {
             </div>
           </div>
         </div>
+
+        <hr />
+
+        {/* get country by id endpoint */}
+        <div className="side-by-side-boxes">
+          {/* left box */}
+          <div className="box-left">
+            <h2>Retrieve country by id</h2>
+            <p>
+              Retrieves detailed information for a specific country using its unique identifier (ID). The response includes data such as the country's
+              name, population, currency, capital, and region. The country is identified based on the provided numeric ID. For a sample request, see
+              Fetching country data by ID.
+            </p>
+
+            <br />
+            <br />
+
+            <h3>Parameters</h3>
+            <hr />
+            <p>
+              <strong>id </strong>
+              &nbsp;&nbsp;&nbsp; mandatory, numerical
+            </p>
+            <p>This specifies which country to get.</p>
+            <br />
+            <br />
+
+            <h3>Returns</h3>
+            <hr />
+            <p>Returns details of a single country by ID, including names, capital, languages, flag, currency, and timestamps.</p>
+          </div>
+
+          {/* right box */}
+          <div className="box-right">
+            {/* request box */}
+            <div className="request-box">
+              <div className="request-line">
+                <h5>
+                  <span className="method">GET</span> {apiUrl}/api/v1/country/:id
+                </h5>
+              </div>
+              <div className="curl-line">
+                <p>
+                  curl --location 'http://localhost:8000/api/v1/country/1' \
+                  <br />
+                  --header 'x-api-key: YOUR_API_KEY_HERE' \
+                </p>
+              </div>
+            </div>
+
+            {/* response box */}
+            <div className="response-box">
+              <div className="response-line">
+                <h5>RESPONSE</h5>
+              </div>
+              <div className="data-line">
+                <pre>{`{
+        "success": true,
+        "response": {
+                "status": 200,
+                "data": {
+                        "country": {
+                                "id": 1,
+                                "officialName": "American Samoa",
+                                "commonName": "American Samoa",
+                                "commonName": "American Samoa",
+                                "languages": {
+                                        "eng": "English",
+                                        "smo": "Samoan"
+                                },
+                                "flagUrl": "https://flagcdn.com/as.svg",
+                                "createdAt": "2025-04-10T20:10:21.164Z",
+                                "updatedAt": "2025-04-10T20:10:21.164Z",
+                                "currency": {
+                                        "id": 16,
+                                        "name": "United States dollar",
+                                        "code": "USD",
+                                        "symbol": "$",
+                                        "createdAt": "2025-04-10T20:10:18.216Z",
+                                        "updatedAt": "2025-04-10T20:10:18.216Z"
+                                }
+                        }
+                }
+        }                         
+}`}</pre>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <hr />
       </div>
     </div>
   );
